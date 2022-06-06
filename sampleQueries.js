9562fc251021aa5a393064df48326682e241db20
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-06-06 13:27:12 -0300

    feat: Add example

--- sampleQueries.js
+++ sampleQueries.js
@@ -26,5 +26,28 @@ var sampleQueries = {
             "children": []
         }
     ]
+},
+"example.2":
+{
+  "distinct": true,
+  "variables": [
+      "?this",
+      "?Disease_1"
+  ],
+  "defaultLang": "en",
+  "order": null,
+  "branches": [
+      {
+          "line": {
+              "s": "?this",
+              "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#may_prevent",
+              "o": "?Disease_1",
+              "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Treatment",
+              "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Disease",
+              "values": []
+          },
+          "children": []
+      }
+  ]
 }
 };
