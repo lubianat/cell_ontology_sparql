c56269013898cb899b155fca5845db522e677c3d
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-09-15 17:08:58 -0300

    Add preliminary test of Sparnatural with the Ubergraph.

--- sampleQueries.js
+++ sampleQueries.js
@@ -1,53 +1,48 @@
 var sampleQueries = {
-	"example.1" :
+  "example.1":
   {
     "distinct": true,
     "variables": [
-        "?disease",
-        "?symptom"
+      "?this",
+      "?Text_1"
     ],
     "defaultLang": "en",
     "order": null,
     "branches": [
-        {
-            "line": {
-                "s": "?disease",
-                "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#has_symptom",
-                "o": "?symptom",
-                "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Disease",
-                "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Symptom",
-                "values": [
-                    {
-                        "label": "abdominal cramps",
-                        "uri": "http://www.wikidata.org/entity/Q3002092"
-                    }
-                ]
-            },
-            "children": []
-        }
+      {
+        "line": {
+          "s": "?this",
+          "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#label",
+          "o": "?Text_1",
+          "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Cell_Type",
+          "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Text",
+          "values": []
+        },
+        "children": []
+      }
     ]
-},
-"example.2":
-{
-  "distinct": true,
-  "variables": [
+  },
+  "example.2":
+  {
+    "distinct": true,
+    "variables": [
       "?this",
       "?Disease_1"
-  ],
-  "defaultLang": "en",
-  "order": null,
-  "branches": [
+    ],
+    "defaultLang": "en",
+    "order": null,
+    "branches": [
       {
-          "line": {
-              "s": "?this",
-              "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#may_prevent",
-              "o": "?Disease_1",
-              "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Treatment",
-              "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Disease",
-              "values": []
-          },
-          "children": []
+        "line": {
+          "s": "?this",
+          "p": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#may_prevent",
+          "o": "?Disease_1",
+          "sType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Treatment",
+          "oType": "http://www.semanticweb.org/lubianat/ontologies/2022/4/sparnanatural_wisecube#Disease",
+          "values": []
+        },
+        "children": []
       }
-  ]
-}
+    ]
+  }
 };
